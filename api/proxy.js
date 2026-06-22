0b8483f51c324be1ccb96514b358266e627e6467
kanchanchakravarti07-hub <kanchanchakravarti07@gmail.com>
2026-06-23 00:10:08 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,10 +5,14 @@ module.exports = async (req, res) => {
   
   if (!targetUrl) return res.status(400).send('Missing URL');
 
+  // Add these headers to allow your GitHub site to access the proxy
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+
   try {
     const response = await fetch(targetUrl, {
       headers: {
-        'Referer': 'https://iptv-eldbert.xyz/',
+        'Referer': 'https://1nyaler.streamhostingcdn.top/',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)'
       }
     });
@@ -17,6 +21,6 @@ module.exports = async (req, res) => {
     res.setHeader('Content-Type', response.headers.get('content-type'));
     res.send(data);
   } catch (err) {
-    res.status(500).send('Error fetching stream: ' + err.message);
+    res.status(500).send('Error: ' + err.message);
   }
 };
